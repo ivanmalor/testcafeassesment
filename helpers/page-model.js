de12235084678cdb79a276eb6e549d189a7b23e5
ironmalo <85460866+ironmalo@users.noreply.github.com>
2021-06-19 21:43:44 -0500

    added page model

---
+++ helpers/page-model.js
@@ -0,0 +1,13 @@
+import { Selector } from 'testcafe';
+
+class Page {
+    constructor () {
+        this.devices = Selector('div.list-devices').find('div.device-main-box');
+        this.deviceName = 'div.device-info > span.device-name';
+        this.deviceType = 'div.device-info > span.device-type';
+        this.deviceCapacity = 'div.device-info > span.device-capacity';
+        this.editDeviceButton = 'div.device-options > a.device-edit';
+        this.removeDeviceButton = 'div.device-options > button.device-remove';
+    }
+}
+export default new Page(); 
