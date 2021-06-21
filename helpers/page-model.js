f83c6a604d93b0f2a6a42c8dda45cc8ec65872e8
Ivan Malo <matheumalo@gmail.com>
2021-06-21 11:47:48 -0500

    refactored selectors in page model

--- helpers/page-model.js
+++ helpers/page-model.js
@@ -1,13 +1,39 @@
 import { ClientFunction, Selector } from 'testcafe';
 
+//CSS Selectors
+const DevicesCss = 'div.list-devices > div.device-main-box';
+const deviceInfoCss = 'div.device-info';
+const deviceOptionsCss = 'div.device-options';
+
 class Page {
     constructor() {
-        this.devices = Selector('div.list-devices').find('div.device-main-box');
-        this.deviceName = 'div.device-info > span.device-name';
-        this.deviceType = 'div.device-info > span.device-type';
-        this.deviceCapacity = 'div.device-info > span.device-capacity';
-        this.editDeviceButton = 'div.device-options > a.device-edit';
-        this.removeDeviceButton = 'div.device-options > button.device-remove';
+        this.devices = Selector(DevicesCss);
+        this.deviceName = Selector(DevicesCss).child(deviceInfoCss).child('span.device-name')
+        this.deviceType = Selector(DevicesCss).child(deviceInfoCss).child('span.device-type');
+        this.deviceCapacity = Selector(DevicesCss).child(deviceInfoCss).child('span.device-capacity');
+        this.editDeviceBtn = Selector(DevicesCss).child(deviceOptionsCss).child('a.device-edit');
+        this.removeDeviceBtn = Selector(DevicesCss).child(deviceOptionsCss).child('button.device-remove');
+    }
+
+    async isDeviceInfoVisible(number) {
+        return await this.deviceName.nth(number).visible
+            || this.deviceType.nth(number).visible
+            || this.deviceCapacity.nth(number).visible;
+    }
+    async getDeviceName(number) {
+        return await this.deviceName.nth(number).textContent
+    }
+
+    async getDeviceType(number) {
+        return await this.deviceType.nth(number).textContent
+    }
+
+    async getDeviceCapacity(number) {
+        return await this.deviceCapacity.nth(number).textContent
+    }
+
+    async deviceButtonsExists(number) {
+        return await this.editDeviceBtn.nth(number).exists || this.removeDeviceBtn.nth(number).exists
     }
 
     async refresh() {
--- tests/test1.js
+++ tests/test1.js
@@ -11,27 +11,14 @@ fixture`List of Devices`
 
 test('Test1 - List of devices', async t => {
     const expectedDevices = t.ctx.devices;
-
     const numberOfDevices = await page.devices.count;
 
     for (let i = 0; i < numberOfDevices; i++) {
-        let device = page.devices.nth(i);
-
-        let deviceName = device.find(page.deviceName);
-        let deviceType = device.find(page.deviceType);
-        let deviceCapacity = device.find(page.deviceCapacity);
-
-        let editDeviceButtonExists = device.find(page.editDeviceButton).exists;
-        let removeDeviceButtonExists = device.find(page.removeDeviceButton).exists;
-
         await t
-            .expect(deviceName.visible).ok()
-            .expect(deviceName.textContent).eql(expectedDevices[i].system_name)
-            .expect(deviceType.visible).ok()
-            .expect(deviceType.textContent).eql(expectedDevices[i].type)
-            .expect(deviceCapacity.visible).ok()
-            .expect(deviceCapacity.textContent).eql(expectedDevices[i].hdd_capacity + ' GB')
-            .expect(editDeviceButtonExists).ok()
-            .expect(removeDeviceButtonExists).ok();
+            .expect(await page.isDeviceInfoVisible(i)).ok()
+            .expect(await page.getDeviceName(i)).eql(expectedDevices[i].system_name)
+            .expect(await page.getDeviceType(i)).eql(expectedDevices[i].type)
+            .expect(await page.getDeviceCapacity(i)).eql(expectedDevices[i].hdd_capacity + ' GB')
+            .expect(page.deviceButtonsExists).ok();
     }
 });
