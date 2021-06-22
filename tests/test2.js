229037c81ba87660dc3ccbe38542f1b4db0d7fb7
Ivan Malo <matheumalo@gmail.com>
2021-06-22 16:53:30 -0500

    added assertions

--- tests/test2.js
+++ tests/test2.js
@@ -5,7 +5,7 @@ import { getIndexOfDevice, sortByDeviceCapacity } from '../helpers/utils.js'
 
 const faker = require('faker');
 
-fixture`List of Devices`
+fixture`Create Device`
     .page('http:localhost:3001')
 
 test('Test2 - Create devices', async t => {
@@ -16,11 +16,11 @@ test('Test2 - Create devices', async t => {
     await t.click(page.addDeviceBtn)
 
     let getLocation = await page.getLocation();
-    
+
     await t
         .expect(getLocation()).contains('/devices/add')
         .expect(deviceFormPage.title.textContent).eql('NEW DEVICE');
-        
+
     await deviceFormPage.enterSystemName(systemName);
     await deviceFormPage.enterHddCapacity(hddCapacity);
     await deviceFormPage.selectDeviceType(deviceType);
@@ -35,5 +35,7 @@ test('Test2 - Create devices', async t => {
 
     await t
         .expect(await page.isDeviceInfoVisible(index)).ok()
-        .expect(await page.getDeviceName(index)).eql(sortedDevices[index].system_name);
+        .expect(await page.getDeviceName(index)).eql(sortedDevices[index].system_name)
+        .expect(await page.getDeviceType(index)).eql(sortedDevices[index].type)
+        .expect(await page.getDeviceCapacity(index)).eql(sortedDevices[index].hdd_capacity + ' GB');
 });
